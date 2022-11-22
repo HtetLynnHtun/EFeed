786c2cd5cad01045de5fce2ead32626cab7a9f24
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2022-11-22 15:10:24 +0630

    Create `FeedItem` model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Htet LynnHtun on 22/11/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
