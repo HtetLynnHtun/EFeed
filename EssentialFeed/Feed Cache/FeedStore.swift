c3015f51043203e17eecd0f0f66297ea1c9b1c27
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2022-12-13 10:50:30 +0630

    Move `FeedStore` to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,14 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Htet LynnHtun on 13/12/2022.
+//
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
