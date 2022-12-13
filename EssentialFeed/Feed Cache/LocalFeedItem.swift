a9d81a9c801fa51405f4a8638715985447ca480b
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2022-12-13 14:06:27 +0630

    Move `LocalFeedItem` to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,20 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Htet LynnHtun on 13/12/2022.
+//
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
