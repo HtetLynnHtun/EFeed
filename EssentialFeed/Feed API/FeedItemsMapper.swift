f1502da3ca68201ddb016198f7a9554c8a782b9f
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2022-12-13 14:05:11 +0630

    Move `RemoteFeedItem` to its own file

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -24,13 +24,4 @@ internal class FeedItemsMapper {
     private struct Root: Decodable {
         let items: [RemoteFeedItem]
     }
-
-    
-}
-
-internal struct RemoteFeedItem: Decodable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
 }
